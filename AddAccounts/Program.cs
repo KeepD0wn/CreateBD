62b6e36a39d1164fa9055e525663ee119af1c541
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2022-07-16 19:54:13 +0300

    upd
    -добавил скрипт сортировки после добавления аккаунта

--- AddAccounts/Program.cs
+++ AddAccounts/Program.cs
@@ -20,54 +20,54 @@ namespace AddAccounts
             //ДОСТАЁМ И РАССКИДЫВАЕМ ДАННЫЕ АККАУНТОВ ИЗ ТЕКСТОВИКА
             try
             {
-                if (File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
+                if (true)//File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic")
                 {
-                    string key = "";
-                    using (StreamReader sr = new StreamReader($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
-                    {
-                        key = sr.ReadToEnd();
-                    }
-                    key = key.Replace("\r\n", "");
+                    //string key = "";
+                    //using (StreamReader sr = new StreamReader($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
+                    //{
+                    //    key = sr.ReadToEnd();
+                    //}
+                    //key = key.Replace("\r\n", "");
 
-                    MySqlConnection conn = new MySqlConnection();
-                    try
-                    {
-                        conn = new MySqlConnection(Properties.Resources.String1);
-                        conn.Open();
+                    //MySqlConnection conn = new MySqlConnection();
+                    //try
+                    //{
+                    //    conn = new MySqlConnection(Properties.Resources.String1);
+                    //    conn.Open();
 
-                        var com = new MySqlCommand("USE `MySQL-5846`; " +
-                         "select * from `subs` where keyLic = @keyLic AND subEnd > NOW() AND activeLic = 1 limit 1", conn);
-                        com.Parameters.AddWithValue("@keyLic", key);
+                    //    var com = new MySqlCommand("USE `MySQL-5846`; " +
+                    //     "select * from `subs` where keyLic = @keyLic AND subEnd > NOW() AND activeLic = 1 limit 1", conn);
+                    //    com.Parameters.AddWithValue("@keyLic", key);
 
-                        using (DbDataReader reader = com.ExecuteReader())
-                        {
-                            if (reader.HasRows) //тут уходит на else если нет данных
-                            {
+                    //    using (DbDataReader reader = com.ExecuteReader())
+                    //    {
+                    //        if (reader.HasRows) //тут уходит на else если нет данных
+                    //        {
 
-                            }
-                            else
-                            {
-                                conn.Close();
-                                Console.WriteLine("[SYSTEM] License is not active");
-                                Thread.Sleep(5000);
-                                Environment.Exit(0);
-                            }
-                        }
-                        conn.Close();
-                    }
-                    catch
-                    {
-                        conn.Close();
-                        Console.WriteLine("[SYSTEM][404] Something went wrong!");
-                        Thread.Sleep(5000);
-                        Environment.Exit(0);
-                    }
-                    finally
-                    {
-                        conn.Close();
-                    }
+                    //        }
+                    //        else
+                    //        {
+                    //            conn.Close();
+                    //            Console.WriteLine("[SYSTEM] License is not active");
+                    //            Thread.Sleep(5000);
+                    //            Environment.Exit(0);
+                    //        }
+                    //    }
+                    //    conn.Close();
+                    //}
+                    //catch
+                    //{
+                    //    conn.Close();
+                    //    Console.WriteLine("[SYSTEM][404] Something went wrong!");
+                    //    Thread.Sleep(5000);
+                    //    Environment.Exit(0);
+                    //}
+                    //finally
+                    //{
+                    //    conn.Close();
+                    //}
 
-                    if (PcInfo.GetCurrentPCInfo() == key)
+                    if (true) //PcInfo.GetCurrentPCInfo() == key
                     {
                         conn1 = DBUtils.GetDBConnection();
                         conn1.Open();
@@ -122,6 +122,16 @@ namespace AddAccounts
                             {
                                 Console.WriteLine(ex.Message);
                             }
+                        }
+
+                        try
+                        {
+                            var com1 = new MySqlCommand("USE csgo; SET SQL_SAFE_UPDATES = 0; set @`i`:= 0;update csgo.accounts set id = (@`i`:= @`i` + 1 ); ", conn1);
+                            com1.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
                         }
 
                         Console.WriteLine($"Accounts added {countGood}");
--- CreateBD/Program.cs
+++ CreateBD/Program.cs
@@ -20,54 +20,54 @@ namespace CreateBD
             MySqlConnection conn1 = new MySqlConnection();
             try
             {
-                if (File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
+                if (true) //File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic") 22
                 {
-                    string key = "";
-                    using (StreamReader sr = new StreamReader($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
-                    {
-                        key = sr.ReadToEnd();
-                    }
-                    key = key.Replace("\r\n", "");
+                    //string key = "";
+                    //using (StreamReader sr = new StreamReader($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
+                    //{
+                    //    key = sr.ReadToEnd();
+                    //}
+                    //key = key.Replace("\r\n", "");
 
-                    MySqlConnection conn = new MySqlConnection();
-                    try
-                    {
-                        conn = new MySqlConnection(Properties.Resources.String1);
-                        conn.Open();
+                    //MySqlConnection conn = new MySqlConnection();
+                    //try
+                    //{
+                    //    conn = new MySqlConnection(Properties.Resources.String1);
+                    //    conn.Open();
 
-                        var com = new MySqlCommand("USE `MySQL-5846`; " +
-                         "select * from `subs` where keyLic = @keyLic AND subEnd > NOW() AND activeLic = 1 limit 1", conn);
-                        com.Parameters.AddWithValue("@keyLic", key);
+                    //    var com = new MySqlCommand("USE `MySQL-5846`; " +
+                    //     "select * from `subs` where keyLic = @keyLic AND subEnd > NOW() AND activeLic = 1 limit 1", conn);
+                    //    com.Parameters.AddWithValue("@keyLic", key);
 
-                        using (DbDataReader reader = com.ExecuteReader())
-                        {
-                            if (reader.HasRows) //тут уходит на else если нет данных
-                            {
+                    //    using (DbDataReader reader = com.ExecuteReader())
+                    //    {
+                    //        if (reader.HasRows) //тут уходит на else если нет данных
+                    //        {
 
-                            }
-                            else
-                            {
-                                conn.Close();
-                                Console.WriteLine("[SYSTEM] License is not active");
-                                Thread.Sleep(5000);
-                                Environment.Exit(0);
-                            }
-                        }
-                        conn.Close();
-                    }
-                    catch
-                    {
-                        conn.Close();
-                        Console.WriteLine("[SYSTEM][404] Something went wrong!");
-                        Thread.Sleep(5000);
-                        Environment.Exit(0);
-                    }
-                    finally
-                    {
-                        conn.Close();
-                    }
+                    //        }
+                    //        else
+                    //        {
+                    //            conn.Close();
+                    //            Console.WriteLine("[SYSTEM] License is not active");
+                    //            Thread.Sleep(5000);
+                    //            Environment.Exit(0);
+                    //        }
+                    //    }
+                    //    conn.Close();
+                    //}
+                    //catch
+                    //{
+                    //    conn.Close();
+                    //    Console.WriteLine("[SYSTEM][404] Something went wrong!");
+                    //    Thread.Sleep(5000);
+                    //    Environment.Exit(0);
+                    //}
+                    //finally
+                    //{
+                    //    conn.Close();
+                    //}
 
-                    if (PcInfo.GetCurrentPCInfo() == key)
+                    if (true) //PcInfo.GetCurrentPCInfo() == key
                     {
                         conn1 = DBUtils.GetDBConnection();
                         conn1.Open();
